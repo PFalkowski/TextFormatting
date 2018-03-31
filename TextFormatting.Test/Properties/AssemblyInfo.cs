175bf4590669d76fa97d536a6adf7a5269a5c560
PFalkowki <Piotr.Falkowski.fm@gmail.com>
2018-03-31 22:27:59 +0200

    initial

---
+++ TextFormatting.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TextFormatting.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextFormatting.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca91d4ae-1932-4d32-9682-0866df48b00f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- TextFormatting/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TextFormatting
-{
-    public class Class1
-    {
-    }
-}
